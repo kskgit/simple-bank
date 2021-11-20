3c48435a9ad6c6d093589d2c2f2c610fb6b40ea3
keisuke <keisuke@diffeasy.com>
2021-11-20 23:07:38 +0900

    add TestGetAccout

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -4,23 +4,42 @@ import (
 	"context"
 	"testing"
 
+	"github.com/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
-		Owner:    "tom",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RnadomMoney(),
+		Currency: util.RandomCurrency(),
 	}
-	accounts, err := testQueries.CreateAccount(context.Background(), arg)
+	account, err := testQueries.CreateAccount(context.Background(), arg)
 	require.NoError(t, err)
-	require.NotEmpty(t, accounts)
+	require.NotEmpty(t, account)
 
-	require.Equal(t, arg.Owner, accounts.Owner)
-	require.Equal(t, arg.Balance, accounts.Balance)
-	require.Equal(t, arg.Balance, accounts.Balance)
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
 
-	require.NotZero(t, accounts.ID)
-	require.NotZero(t, accounts.CreatedAt)
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
 }
