80b52079467d3af5e48c4febb840be945928cf18
keisuke nakao <ksk@keisukes-MacBook-Air.local>
2022-01-08 15:32:20 +0900

    add delete account

--- api/server.go
+++ api/server.go
@@ -18,6 +18,7 @@ func NewServer(store *db.Store) *Server {
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.PATCH("/accounts/:id", server.updateAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
 
 	server.router = router
 	return server
