e444176d4e5cdb1a5ccd8f5c493b03ceeeb3f7cf
nakao keisuke <r3grashopper193@gmail.com>
2022-12-26 23:08:22 +0900

    fix env key

--- util/config.go
+++ util/config.go
@@ -8,7 +8,7 @@ import (
 
 type Config struct {
 	DBDriver            string        `mapstructure:"DB_DRIVER"`
-	DBSource            string        `mapstructure:"DB_SORCE"`
+	DBSource            string        `mapstructure:"DB_SOURCE"`
 	ServerAddress       string        `mapstructure:"SEVER_ADDRESS"`
 	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
 	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
